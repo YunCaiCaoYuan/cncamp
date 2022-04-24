e60c607e635cc32318645ab11536ac1cab6edf06
sunbin <>
2022-04-24 14:50:09 +0800

    模块二作业

--- mod_two/main.go
+++ mod_two/main.go
@@ -3,14 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
+const (
+	VERSION = "VERSION"
+)
+
+// 1、接收客户端 request，并将 request 中带的 header 写入 response header
 func newRequest1() http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		for _, item := range request.Header {
-			writer.Header()
+		logAccess(request)
+		for k, v := range request.Header {
+			writer.Header().Set(k, strings.Join(v, ";"))
 		}
-		fmt.Fprintln(writer, "123")
+	})
+}
+
+// 2、读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+func newRequest2() http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		logAccess(request)
+		writer.Header().Set(VERSION, os.Getenv(VERSION))
+	})
+}
+
+// 3、Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
+func logAccess(request *http.Request) {
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), request.RemoteAddr)
+}
+
+// 4、当访问 localhost/healthz 时，应返回 200
+func healthz() http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		logAccess(request)
+		writer.WriteHeader(200)
 	})
 }
 
@@ -18,5 +47,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/request1", newRequest1())
-	http.ListenAndServe(":8080", mux)
+	mux.Handle("/request2", newRequest2())
+	mux.Handle("/healthz", healthz())
+	_ = http.ListenAndServe(":80", mux)
 }
