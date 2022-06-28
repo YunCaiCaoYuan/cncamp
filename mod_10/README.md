692465afbeb62d1f6cf318a0c4093e909499d04d
sunbin <>
2022-06-28 13:11:21 +0800

    模块十作业

--- mod_10/README.md
+++ mod_10/README.md
@@ -1,3 +1,5 @@
 ## 修改记录:
     1、为 HTTPServer 添加 0-2 秒的随机延时
-    已在mod_two修改记录；
+    已在mod_two添加；
+    2、为 HTTPServer 项目添加延时 Metric；
+    已在mod_two添加；
--- mod_two/main.go
+++ mod_two/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 	"math/rand"
+	"mod_two/metrics"
 	"net/http"
 	"os"
 	"os/signal"
@@ -20,8 +22,9 @@ const (
 // 1、接收客户端 request，并将 request 中带的 header 写入 response header
 func newRequest1() http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		timer := metrics.NewTimer()
+		defer timer.ObserveTotal()
 		logAccess(request)
-		sleepRand()
 		for k, v := range request.Header {
 			writer.Header().Set(k, strings.Join(v, ";"))
 		}
@@ -31,8 +34,9 @@ func newRequest1() http.Handler {
 // 2、读取当前系统的环境变量中的 VERSION 配置，并写入 response header
 func newRequest2() http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		timer := metrics.NewTimer()
+		defer timer.ObserveTotal()
 		logAccess(request)
-		sleepRand()
 		writer.Header().Set(VERSION, os.Getenv(VERSION))
 	})
 }
@@ -40,23 +44,25 @@ func newRequest2() http.Handler {
 // 3、Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
 func logAccess(request *http.Request) {
 	logger.Info("logAccess", zap.Any("request", request))
-	sleepRand()
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), request.RemoteAddr, "200")
+	sleepRand()
 }
 
 // 4、当访问 localhost/healthz 时，应返回 200
 func healthz() http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		timer := metrics.NewTimer()
+		defer timer.ObserveTotal()
 		logAccess(request)
-		sleepRand()
 		writer.Write([]byte("200"))
 	})
 }
 
 func livez() http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		timer := metrics.NewTimer()
+		defer timer.ObserveTotal()
 		logAccess(request)
-		sleepRand()
 		writer.Write([]byte("200"))
 	})
 }
@@ -81,12 +87,14 @@ var logger *zap.Logger
 func main() {
 	logger, _ = zap.NewProduction()
 	logger.Info("server start!")
+	metrics.Register()
 
 	mux := http.NewServeMux()
 	mux.Handle("/request1", newRequest1())
 	mux.Handle("/request2", newRequest2())
 	mux.Handle("/healthz", healthz())
 	mux.Handle("/livez", livez())
+	mux.Handle("/metrics", promhttp.Handler())
 	//_ = http.ListenAndServe(":80", mux)
 	server := &http.Server{Addr: ":80", Handler: mux}
 
